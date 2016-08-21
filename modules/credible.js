e812e45a9be3f44e51f93fa88c6d756527c78e44
Robert Stanfield <robert@stanfield.it>
2016-08-20 20:22:46 -0400

    Too much money not enough bitches, gotta lower that interest rate i guess

--- modules/credible.js
+++ modules/credible.js
@@ -1,14 +1,14 @@
 module.exports = function(loans) {
   loans = loans.filter(function(loan) {
     var year = loan.empLength >= 1;
-  	var type = loan.purpose == "debt_consolidation" || loan.purpose === "home_improvement";
-  	var notDirty = loan.accNowDelinq === 0 && loan.delinq2Yrs === 0 && loan.mthsSinceLastMajorDerog === null &&
+    var type = loan.purpose == "debt_consolidation" || loan.purpose === "home_improvement";
+    var notDirty = loan.accNowDelinq === 0 && loan.delinq2Yrs === 0 && loan.mthsSinceLastMajorDerog === null &&
                    loan.pubRec === 0 && loan.pubRecBankruptcies === 0 && loan.mthsSinceLastDelinq === null;
-  	var credit = loan.ficoRangeLow >= 650;
-  	var lessOpenAccounts = loan.totalAcc > loan.openAcc;
+    var credit = loan.ficoRangeLow >= 650;
+    var lessOpenAccounts = loan.totalAcc > loan.openAcc;
 
-  	var reasonableAmount = (loan.totCurBal <= loan.loanAmount + 5000) || (loan.totCurBal > loan.loanAmount);
-  	var intRate = loan.intRate >= 13 && (loan.purpose !== "home_improvement"
+    var reasonableAmount = (loan.totCurBal <= loan.loanAmount + 5000) || (loan.totCurBal > loan.loanAmount);
+    var intRate = loan.intRate >= 12 && (loan.purpose !== "home_improvement"
                || (loan.purpose === "home_improvement" && loan.intRate <= 14));
 
     var noCollections = loan.totCollAmt === 0 && loan.taxLiens === 0;
